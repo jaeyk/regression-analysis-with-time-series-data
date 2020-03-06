efb1fd44e501ba7ad5020d939bd28a71a45844b4
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2020-03-06 13:18:17 -0800

    updated data viz

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # analyzing-latino-asian-civic-infrastructure
 Analyzing Asian American and Latino Advocacy and Community-based organizations
+
+========================================================================
+                                         Dependent variable:
+                              ------------------------------------------
+                                   log(Freq)                   log(Freq)
+                                      OLS          coefficient  MM-type
+                                                      test      linear
+                                      (1)              (2)        (3)
+------------------------------------------------------------------------
+Percentage                          0.293***        0.293***   0.295***
+                                    (0.107)          (0.075)    (0.076)
+
+pop_percentage                       -0.028          -0.028     -0.035
+                                    (0.159)          (0.101)    (0.124)
+
+categoryLatino                       0.484            0.484      0.525
+                                    (0.557)          (0.367)    (0.456)
+
+presidency                           -0.020          -0.020     -0.012
+                                    (0.189)          (0.171)    (0.197)
+
+senate
+
+
+house
+
+
+Constant                             -0.551          -0.551*    -0.560*
+                                    (0.425)          (0.286)    (0.299)
+
+------------------------------------------------------------------------
+Observations                           46                         46
+R2                                   0.259                       0.250
+Adjusted R2                          0.187                       0.177
+Residual Std. Error (df = 41)        0.631                       0.685
+F Statistic                   3.589** (df = 4; 41)
+========================================================================
+Note:                                        *p<0.1; **p<0.05; ***p<0.01
---
+++ functions/visualize_its.r
@@ -0,0 +1,163 @@
+
+ols_its <- function(input){
+  
+  # Apply model
+  
+  model <- lm(Freq ~ intervention + Percentage + pop_percentage + category + Type + presidency + senate + house, 
+               data = input)
+  
+  # Make predictions 
+  
+  input$pred <- predict(model, type = "response", input)
+  
+  # Create confidence intervals  
+  
+  ilink <- family(model)$linkinv # Extracting the inverse link from parameter objects 
+  
+  # Combined prediction outputs 
+  
+  input <-predict(model, input, se.fit = TRUE)[1:2] %>%
+    bind_cols(input) %>%
+    mutate(
+      upr = ilink(fit + (2 * se.fit)),
+      lwr = ilink(fit - (2 * se.fit)))
+  
+  # Visualize the outcome 
+  
+  input %>%
+    ggplot(aes(x = Year, y = Freq)) +
+    geom_point(alpha = 0.2) +
+    geom_line(aes(y = pred), size = 1) +
+    
+    geom_vline(xintercept = c(1980), linetype = "dashed", size = 1, color = "red") +
+    ggthemes::theme_base() +
+    labs(x = "Year",
+         y = "Organizational founding") +
+    geom_ribbon(aes(ymin = lwr, ymax = upr),
+                alpha = 0.3) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))  +
+    facet_grid(category~Type)
+}
+
+ps_its <- function(input){
+  
+  # Apply poisson
+  
+  model <- glm(Freq ~ intervention + Percentage + pop_percentage + category + Type + presidency + senate + house, 
+              data = input, family = "poisson")
+  
+  # Make predictions 
+  
+  input$pred <- predict(model, type = "response", input)
+  
+  # Create confidence intervals  
+  
+  ilink <- family(model)$linkinv # Extracting the inverse link from parameter objects 
+  
+  # Combined prediction outputs 
+  
+  input <-predict(model, input, se.fit = TRUE)[1:2] %>%
+    bind_cols(input) %>%
+    mutate(
+      upr = ilink(fit + (2 * se.fit)),
+      lwr = ilink(fit - (2 * se.fit)))
+  
+  # Visualize the outcome 
+  
+  input %>%
+    ggplot(aes(x = Year, y = Freq)) +
+    geom_point(alpha = 0.2) +
+    geom_line(aes(y = pred), size = 1) +
+    
+    geom_vline(xintercept = c(1980), linetype = "dashed", size = 1, color = "red") +
+    ggthemes::theme_base() +
+    labs(x = "Year",
+         y = "Organizational founding") +
+    geom_ribbon(aes(ymin = lwr, ymax = upr),
+                alpha = 0.3) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+    facet_grid(category~Type)
+}
+
+nb_its <- function(input){
+  
+  # Apply model
+  
+  model <- glm.nb(Freq ~ intervention + Percentage + pop_percentage + category + 
+                      Type + presidency + senate + house, 
+                    data = input)
+  
+  # Make predictions 
+  
+  input$pred <- predict(model, type = "response", input)
+  
+  # Create confidence intervals  
+  
+  ilink <- family(model)$linkinv # Extracting the inverse link from parameter objects 
+  
+  # Combined prediction outputs 
+  
+  input <-predict(model, input, se.fit = TRUE)[1:2] %>%
+    bind_cols(input) %>%
+    mutate(
+      upr = ilink(fit + (2 * se.fit)),
+      lwr = ilink(fit - (2 * se.fit)))
+  
+  # Visualize the outcome 
+  
+  input %>%
+    ggplot(aes(x = Year, y = Freq)) +
+    geom_point(alpha = 0.2) +
+    geom_line(aes(y = pred), size = 1) +
+    
+    geom_vline(xintercept = c(1980), linetype = "dashed", size = 1, color = "red") +
+    ggthemes::theme_base() +
+    labs(x = "Year",
+         y = "Organizational founding") +
+    geom_ribbon(aes(ymin = lwr, ymax = upr),
+                alpha = 0.3) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+    facet_grid(category~Type)
+}
+
+zerofinl_its <- function(input){
+  
+  # Apply model
+  
+  model <- zeroinfl(Freq ~ intervention + Percentage + pop_percentage + category + 
+                      Type + presidency + senate + house | intervention + Percentage + pop_percentage + category + 
+                      Type + presidency + senate + house, dist = "negbin",
+               data = input)
+  
+  # Make predictions 
+  
+  input$pred <- predict(model, type = "response", input)
+  
+  # Create confidence intervals  
+  
+  ilink <- family(model)$linkinv # Extracting the inverse link from parameter objects 
+  
+  # Combined prediction outputs 
+  
+  input <-predict(model, input, se.fit = TRUE)[1:2] %>%
+    bind_cols(input) %>%
+    mutate(
+      upr = ilink(fit + (2 * se.fit)),
+      lwr = ilink(fit - (2 * se.fit)))
+  
+  # Visualize the outcome 
+  
+  input %>%
+    ggplot(aes(x = Year, y = Freq)) +
+    geom_point(alpha = 0.2) +
+    geom_line(aes(y = pred), size = 1) +
+    
+    geom_vline(xintercept = c(1980), linetype = "dashed", size = 1, color = "red") +
+    ggthemes::theme_base() +
+    labs(x = "Year",
+         y = "Organizational founding") +
+    geom_ribbon(aes(ymin = lwr, ymax = upr),
+                alpha = 0.3) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+    facet_grid(category~Type)
+}
