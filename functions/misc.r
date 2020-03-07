8ce78ff72e9197758469c6a56c12b5d78eb1510e
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2020-03-07 15:26:13 -0800

    org to ts

--- functions/misc.r
+++ functions/misc.r
@@ -5,4 +5,32 @@ merge_by_group <- function(x, y){
     mutate(y, category="Latino"))
 }
 
-                              
+org_to_ts <- function(data){
+  
+  # Create the full years
+  
+  all_years <- seq(min(data$F.year), max(data$F.year))
+  
+  # Turn it into a dataframe 
+  
+  all_years <- data.frame(F.year = all_years)
+  
+  # Count by year and type
+  
+  # Count by year and type
+  
+  data_year <- data %>%
+    count(F.year, States, Type) %>%
+    rename(Freq = n) %>%
+    right_join(all_years, by = "F.year")
+  
+  # Replace NA Freq with 0s
+  
+  data_year$Freq[is.na(data_year$Freq)] <- 0
+  
+  # Replace NA Type with unique Type 
+  
+  data_year$Type[is.na(data_year$Type)] <- unique(data_year$Type[!is.na(data_year$Type)])
+  
+  data_year
+}
--- functions/theme_publications.r
+++ functions/theme_publications.r
@@ -1,15 +1,21 @@
-theme_Publication <- function(base_size=13, base_family="helvetica") {
+
+# I adapted theme publication function from [this blog post](https://rpubs.com/Koundy/71792).
+
+theme_Publication <- function(base_size) {
+  
   library(grid)
   library(ggthemes)
-  (theme_foundation(base_size=base_size, base_family=base_family)
-    + theme(plot.title = element_text(size = rel(1.2), hjust = 0.5),
+  
+  (theme_foundation(base_size = base_size)
+    + theme(plot.title = element_text(size = rel(1.2)),
             text = element_text(),
+            plot.caption = element_text(hjust = 0, face= "italic"), 
+            plot.title.position = "plot", 
+            plot.caption.position =  "plot",
             panel.background = element_rect(colour = NA),
             plot.background = element_rect(colour = NA),
             panel.border = element_rect(colour = NA),
             axis.title = element_text(size = rel(1)),
-            axis.title.y = element_text(angle=90,vjust =2),
-            axis.title.x = element_text(vjust = -0.2),
             axis.text = element_text(), 
             axis.line = element_line(colour="black"),
             axis.ticks = element_line(),
@@ -36,4 +42,26 @@ scale_colour_Publication <- function(...){
   library(scales)
   discrete_scale("colour","Publication",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
   
+}
+
+# I adapted theme map function from this [blog post](https://timogrossenbacher.ch/2016/12/beautiful-thematic-maps-with-ggplot2-only/).
+
+theme_map <- function(...) {
+  theme_base() +
+    theme(
+      axis.line = element_blank(),
+      axis.text.x = element_blank(),
+      axis.text.y = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.grid.major = element_blank(),
+      plot.background = element_blank(), 
+      panel.background = element_blank(), 
+      legend.background = element_blank(),
+      panel.border = element_blank(),
+      legend.position = "bottom",
+      ...
+    )
 }
