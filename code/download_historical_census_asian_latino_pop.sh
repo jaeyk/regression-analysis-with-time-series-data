701ee03d341430943b6334455ca37fcb2dc3fc39
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2020-07-16 02:06:54 -0700

    downloaded another historical census file

---
+++ code/download_historical_census_asian_latino_pop.sh
@@ -0,0 +1,65 @@
+#!/bin/bash 
+
+# Download the Historical Census data
+
+cd ~/analyzing-asian-american-latino-civic-infrastructure/raw_data
+ 
+## South Carolina  
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/sctab.xls 
+
+## South Dakota 
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/sdtab.xls
+
+## Tennesse 
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/tntab.xls
+
+## Texas 
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/txtab.xls
+
+## Utah 
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/uttab.xls
+
+## Vermont 
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/vttab.xls
+
+## Virginia 
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/vatab.xls
+
+## Washington 
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/watab.xls
+
+## West Virginia 
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/wvtab.xls
+
+## Wisconsin 
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/witab.xls
+
+## Wyoming 
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/wytab.xls
+
+## Alabama 
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/altab.xls
+
+## Alaska 
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/aktab.xls
+
+## Arizona 
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/aztab.xls
+
+## Arkansas 
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/artab.xls
+
+## California 
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/catab.xls
+
+## Colorado 
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/cotab.xls
+
+## Conneticut 
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/cttab.xls
+
+## Delaware 
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/dctab.xls
+
+## District of Columbia 
+wget https://www2.census.gov/library/working-papers/2005/demo/pop-twps0076/detab.xls
--- code/move_downloaded_files.sh
+++ code/move_downloaded_files.sh
@@ -1,8 +1,13 @@
 #!/bin/bash 
 
-# Download ACS data 
+# Download the ACS data 
+
+cd ~/analyzing-asian-american-latino-civic-infrastructure/raw_data
+
 wget https://www2.census.gov/library/visualizations/time-series/demo/Poverty-Rates-by-County-1960-2010.xlsm
 
-# Move EOA data 
+# Move the EOA data 
 cp gomf6571.dta.zip cap.dta ~/analyzing-asian-american-latino-civic-infrastructure/raw_data
 cd ~/analyzing-asian-american-latino-civic-infrastructure/raw_data | unzip gomf6571.dta.zip
+
+
