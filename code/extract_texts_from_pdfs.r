378cc6f04e5014103d9517c648a12b2ccfa0c9ff
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2020-07-17 15:18:40 -0700

    update

--- code/extract_texts_from_pdfs.r
+++ code/extract_texts_from_pdfs.r
@@ -13,27 +13,27 @@ filename <- list.files("/home/jae/analyzing-asian-american-latino-civic-infrastr
 
 # Reformat filename 
 date_list <- gsub(".*_", "", filename)
-    
+
 # Extract texts from the PDF
 text_list <- list(filename) %>%
-    pmap(~pdf_text(.)) 
-  
+  pmap(~pdf_text(.)) 
+
 # List into a data frame 
 df <- text_list %>% 
-    map_df(enframe, .id = 'ListElement') 
+  map_df(enframe, .id = 'ListElement') 
 
 # for loop 
 
 for (i in seq(1:length(date_list))){ 
-
-    df$ListElement[df$ListElement == paste(i)] <- date_list[i]
-
-    }
+  
+  df$ListElement[df$ListElement == paste(i)] <- date_list[i]
+  
+}
 
 # Rename and mutate
 df <- df %>%
-    rename("date" = "ListElement",
-           "page_number" = "name")
+  rename("date" = "ListElement",
+         "page_number" = "name")
 
 # Save the df 
 write_rds(df, "/home/jae/analyzing-asian-american-latino-civic-infrastructure/processed_data/nclr_text.rds")
