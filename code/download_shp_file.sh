9f7d4895ed6a88e2403061bb82982df159f65966
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2020-07-19 05:02:52 -0700

    don't need to do county-level geocoding

---
+++ code/download_shp_file.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Change directory
+cd ~/analyzing-asian-american-latino-civic-infrastructur
+e/raw_data
+
+# Download the shape file
+wget http://spatial.lib.berkeley.edu/public/ark28722-s7hs4j/data.zip | unzip
+
+# Remove the file
+rm data.zip 
