b20e1191fad3535510fe8d5b7b5a75f6015edc6f
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2020-07-26 23:04:59 -0700

    corrected the additional robustness check

--- code/parse_historical_census_poverty.r
+++ code/parse_historical_census_poverty.r
@@ -12,43 +12,38 @@ census <- readxl::read_xlsx(here("raw_data", "Poverty-Rates-by-County-1960-2010.
 ) # Cell range
 
 # Filter data
-census <- census[-1, c(2, 3, 4, 
-                       5, 6, 7,
-                       11, 12, 13)] # Skip the USA row and select only 1960 and 1970 data
+census <- census[-1, c(2, 3, 4, # FIPS, State, County
+                       5, 6, 7, # Poverty rate 1960, 1970, 1980
+                       11, 12, 13)] # Pop size 1960, 1970, 1980
+
+# No states!
+census <- census %>% filter(!(length(FIPS) == 1))
 
 # Pivot longer
-census_county <- census %>%
+census_county1 <- census %>%
   pivot_longer(
     cols = contains("Poverty"),
-    names_to = "poverty_year",
+    names_to = "year",
     values_to = "poverty_rate"
-  ) %>%
+  ) 
+
+census_county2 <- census %>%
   pivot_longer(
     cols = contains("Population"),
-    names_to = "pop_year",
+    names_to = "year",
     values_to = "pop_size"
-  ) %>%
+  ) 
 
+census_county <- census_county1 %>%
+  mutate(pop_size = census_county2$pop_size)
+
+census_county <- census_county %>%
   # Extract only numbers
   mutate(
-    poverty_year = parse_number(poverty_year),
-    pop_year = parse_number(pop_year)
-  ) %>%
+    year = parse_number(year)
+  )
 
-  # Rename col
-  rename("year" = "pop_year") %>%
-
-  # Drop col
-  select(-c("poverty_year")) %>%
-
-  # Drop state totals
- # filter(FIPS != 1) %>%
-
-  # Remove duplicates
- #  distinct() %>%
-
-  # Reorder cols
-  select(FIPS, State, County, year, poverty_rate, pop_size)
+census_county <- census_county[,-c(4:6)]
 
 # Export the file
 write_csv(
